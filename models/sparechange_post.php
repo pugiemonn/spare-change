4c427643663bc9de94945f224f33746d1fe5837f
pugiemonn <pugiemonn@gmail.com>
2011-09-09 12:05:09 +0900

    投稿へのバリデーション500円より大きく10万以下

--- models/sparechange_post.php
+++ models/sparechange_post.php
@@ -4,7 +4,20 @@ class SparechangePost extends AppModel
   var $name = "SparechangePost";
   var $validate = array(
     'user_id' => 'notEmpty',
-    'cost'    => 'numeric'
+    'comment' => 'notEmpty',
+    'cost'    => array(
+      //数値形式の判定
+      'numeric' => array(
+        'rule'     => 'numeric',
+        'required' => true,
+        'message'  => 'aaa',
+      ),
+      //500円から10万円まで
+      'range' => array(
+        'rule' => array('range', 500, 100000.1),
+        'message' => 'nurupo'
+      ),
+    )
   );
 }
 ?>
