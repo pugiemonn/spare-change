128c387d50d8d7f7b65e957d4e4a129ca8f45071
pugiemonn <pugiemonn@gmail.com>
2011-10-13 13:40:07 +0900

    ユーザー名の文字数下限を３文字までに

--- models/user.php
+++ models/user.php
@@ -24,8 +24,8 @@ class User extends AppModel {
         'message'  => '半角英数字を入力してください',
       ),
       'between' => array(
-        'rule'    => array('between', 5, 15), 
-        'message' => '5文字から15文字までにしてください',
+        'rule'    => array('between', 3, 15), 
+        'message' => '3文字から15文字までにしてください',
       ),
     ),
     
